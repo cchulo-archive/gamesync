bac252fb8d1bbc550a6193f6cf619c9190b29937
Carlos Chulo <cchulo.dev@gmail.com>
2023-05-05 01:47:38 -0700

    added gamesync-utilities to keep gamesync itself lightweight

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ set -e -o pipefail
 
 echo 'Installing gamesync'
 sudo cp gamesync /usr/local/bin/
+sudo cp gamesync-utilities /usr/local/bin/
 mkdir -p ~/.local/share/gamesync
 echo 'gamesync installed!'
