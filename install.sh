16bc479f7c28faf4a7b224ca82209667f954ea9e
Carlos Chulo <cchulo.dev@gmail.com>
2023-05-28 05:50:55 -0700

    no longer putting all binary files under /usr/local/bin, putting libraries and helper functions inside of /usr/local/share/gamesync

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -e -o pipefail
 
 echo 'Installing gamesync'
 sudo cp gamesync /usr/local/bin/
-sudo cp gamesync-utilities /usr/local/bin/
+sudo mkdir /usr/local/share/gamesync
+sudo cp share/* /usr/local/share/gamesync/
 mkdir -p ~/.local/share/gamesync
 echo 'gamesync installed!'
