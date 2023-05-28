e37d081e8e6f9809b5aac37c7d3434c3d1471855
Carlos Chulo <cchulo.dev@gmail.com>
2023-05-28 06:32:55 -0700

    now creating dedicated logs folder under ~/.local/share/gamesync as well as a saves folder, now reading in variables from ~/.gamesync, no need for environment variables, and checking to make sure syncthing folder exists

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ set -e -o pipefail
 
 echo 'Installing gamesync'
 sudo cp gamesync /usr/local/bin/
-sudo mkdir /usr/local/share/gamesync
+sudo mkdir -p /usr/local/share/gamesync
 sudo cp share/* /usr/local/share/gamesync/
-mkdir -p ~/.local/share/gamesync
+mkdir -p ~/.local/share/gamesync/logs
+mkdir -p ~/.local/share/gamesync/saves
+touch ~/.gamesync
 echo 'gamesync installed!'
